5082e20d4c42c10453f7e93ac90229974aa5490c
Daniel L Ferreira <dl.ferreira179@gmail.com>
2020-12-14 11:40:26 -0300

    Fix index method logic for DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Api;
-use App\Models\Plan;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
-        $name = Auth::user()->name;
-        $sms_credits = $user->userplan->SMSCredits;
-        $price_plan = $sms_credits = $user->userplan->SMSCredits;
-        $planid = $user->userplan->planId;
-        $plan = Plan::find($planid);
-        $requests_quantity = $plan->RequestsQuantity;
-        $extras = $this->calculate_extras($sms_credits,$requests_quantity);
+        $name = $user->name;
+
+        $requests_quantity = $this->get_requests_quantity_for($user);
+        $requests_consumed = $this->get_requests_consumed_for($user);
+        $requests_available = $this->calculate_available($requests_consumed, $requests_quantity);
+        $extras = $this->calculate_extras($requests_consumed, $requests_quantity);
 
 
-        return view('dashboard', compact('name', 'sms_credits', 'requests_quantity','extras'));
+        return view('dashboard', compact(
+            'name',
+            'requests_consumed',
+            'requests_quantity',
+            'requests_available',
+            'extras',
+        ));
     }
 
     public function search(Request $request)
@@ -31,14 +34,33 @@ class DashboardController extends Controller
         dd($teste);
     }
 
-    public function calculate_extras($sms_credits, $requests_quantity)
+    private function calculate_extras($consumed, $total)
     {
-        if($requests_quantity > $sms_credits)
+        if($consumed > $total)
         {
-            return $requests_quantity - $sms_credits;
+            return $consumed - $total;
         }
-        else{
+        else {
             return 0;
         }
+    }
+
+    private function get_requests_consumed_for($user) {
+        $user_id = $user->Id;
+        return DB::select("exec dbo.get_api_usage $user_id")[0]->request_count;
+    }
+
+    private function calculate_available($consumed, $total)
+    {
+        if ($total > $consumed) {
+            return $total - $consumed;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    private function get_requests_quantity_for($user) {
+        return $user->userplan->plan->RequestsQuantity;
     }
 }
