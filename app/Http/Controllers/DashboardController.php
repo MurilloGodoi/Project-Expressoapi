b8603bdb1b671aace87e4acc8771e76bba86c832
Daniel L Ferreira <dl.ferreira179@gmail.com>
2020-12-14 16:53:10 -0300

    Add weekly and daily usage analysis

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,6 +18,8 @@ class DashboardController extends Controller
         $requests_available = $this->calculate_available($requests_consumed, $requests_quantity);
         $extras = $this->calculate_extras($requests_consumed, $requests_quantity);
 
+        $weekly_usage = $this->get_weekly_usage_for($user);
+        $daily_usage_mean = $this->calculate_daily_usage_mean($weekly_usage);
 
         return view('dashboard', compact(
             'name',
@@ -25,6 +27,8 @@ class DashboardController extends Controller
             'requests_quantity',
             'requests_available',
             'extras',
+            'weekly_usage',
+            'daily_usage_mean',
         ));
     }
 
@@ -62,5 +66,27 @@ class DashboardController extends Controller
 
     private function get_requests_quantity_for($user) {
         return $user->userplan->plan->RequestsQuantity;
+    }
+
+    private function calculate_daily_usage_mean($weekly_usage) {
+        return number_format(array_sum($weekly_usage)/count($weekly_usage), 1);
+    }
+
+    private function get_weekly_usage_for($user) {
+        $user_id = $user->Id;
+        $query_result = DB::select("exec dbo.get_api_weekly_usage $user_id");
+        $result = [];
+
+        for ($i = 0; $i < 7; $i++) {
+            $days_ago = array_column($query_result, 'days_ago');
+            if ($key = array_search($i, $days_ago)) {
+                array_unshift($result, $query_result[$key]->request_count);
+            }
+            else {
+                array_unshift($result, 0);
+            }
+        }
+
+        return $result;
     }
 }
